9690649fc665371cdcf9a9e264a8bd4098684ca4
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-04-29 07:24:01 +0100

    Continuazione checkout

--- src/checkout.php
+++ src/checkout.php
@@ -4,7 +4,7 @@
 
 <main class="d-flex align-items-center">
     <div class="container shadow-lg rounded-3 p-3 mt-2">
-        <div class="row bg-secondary bg-opacity-25 py-2 px-1 align-items-center">
+        <div class="row bg-light py-2 px-1 align-items-center">
             <div class="col-4">
                 <img src="img/propic-placeholder.jpg" class="rounded-5 thumbnail" style="width: 50px;" alt="" srcset="">
             </div>
@@ -12,27 +12,47 @@
                 <img src="img/vise.svg" style="width: 50px;" alt="" srcset="">
             </div>
             <div class="col-4 text-end">
-                5,55 EUR
+            <span class="badge rounded-pill text-bg-primary fs-6">5,55 EUR</span>
+                
             </div>
         </div>
 
         <div class="row">
-            <div class="col-3 mt-3 ms-3">
-                <strong>Paga con</strong>
+            <div class="col mt-3 mx-3">
+                <strong>Paga con </strong>
                 <form action="" class="my-3">
-                    <div class="form-check mb-2">
-                        <!--<img src="img/credit-card-solid.svg" style="width:25px;"alt="" srcset=""> !-->
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                        <label class="form-check-label" for="flexRadioDefault1">
-                            Default radio
-                        </label>
+                    <div class="row">
+
+                        <div class="form-check mb-2">
+                            <img src="img/credit-card-solid.svg" class="mx-2 " style="width:25px;" alt="" srcset="">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                                Default radio
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <img src="img/credit-card-solid.svg" class="mx-2" style="width:25px;" alt="" srcset="">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                            <label class="form-check-label" for="flexRadioDefault2">
+                                Default checked radio
+                            </label>
+                        </div>
+                        <button class="col-6 col-md-4 col-lg-3 col-xl-3 btn btn-outline-dark mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                            </svg>
+                            Aggiungi carta
+                        </button>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-                        <label class="form-check-label" for="flexRadioDefault2">
-                            Default checked radio
-                        </label>
+                    <div class="row mt-5">
+                        <div class="text-center">
+                            <button class="col-8 col-xl-6 btn btn-primary rounded-pill" type="submit">Conferma acquisto</button>
+                        </div>
+                        <div class="text-center mt-3 ">
+                            <a class="text-decoration-none" href="http://">Annulla il pagamento e torna a [Sito]</a>
+                        </div>
                     </div>
+                    
                 </form>
             </div>
         </div>
