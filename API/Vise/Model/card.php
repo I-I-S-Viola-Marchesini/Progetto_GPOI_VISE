7fcceeeffafc3507061fe7232480858686b7ba74
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-05-04 18:46:20 +0200

    Creazione carta

--- API/Vise/Model/card.php
+++ API/Vise/Model/card.php
@@ -5,8 +5,18 @@ class Card
 
     protected $conn;
 
-    public function __construct($db){
+    public function __construct($db)
+    {
         $this->conn = $db;
+    }
+
+    public function createCard($cardName, $instituteCommunicationToken, $expirationDate, $billingAddressID)
+    {
+        $query = "INSERT INTO vise_db.card (card_name, institute_communication_token, expiration_date, billing_address_id) VALUES (?, ?, ?, ?);";
+        $result = $this->conn->prepare($query);
+        $result->bind_param('sssi', $cardName, $instituteCommunicationToken, $expirationDate, $billingAddressID);
+        $result->execute();
+        return $result;
     }
 
 }
