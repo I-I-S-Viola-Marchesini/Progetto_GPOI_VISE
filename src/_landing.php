e0f11337358617a99a415789ac97b29a8d078460
Angelo Pavan <angelopavan.dev@gmail.com>
2023-05-06 10:11:24 +0200

    Modifica della landing page.

--- src/_landing.php
+++ src/_landing.php
@@ -6,12 +6,25 @@
 </title>
 
 <main class="d-flex align-items-end mt-5">
-    <div class="container text-center shadow-lg rounded-3 py-5 bg-white" style="min-height: 800vh;">
-        <div class="row g-2">
+    <div class="container shadow-lg rounded-3 py-5 bg-white" style="min-height: 800vh;">
+        <div class="row g-2 mt-5 bg-whitesmoke">
 
+            <div class="col-10 mx-auto">
+                <div class="row">
+                    <div class="col-6 my-auto">
+                        <h2 class="text-center mb-4">Con <strong>Vise</strong> inviare denaro è semplice!</h2>
+                        <p>Effettua pagamenti verso chi desideri, che sia un amico o un esercente, l'unico requisito è avere un conto <strong>Vise</strong>.</p>
+                    </div>
+                    
+                    <div class="col-6">
+                        <img src="img/landing-image-phone.png" alt="Immagine Landing Page">
+                    </div>
+                </div>
+            </div>
 
-            <h1 class="display-3">Semplice... è Vise!</h1>
-            <small class="text-body-primary">Landing page per vise</small>
+            <!-- <h1 class="display-3">Semplice... è Vise!</h1>
+            <small class="text-body-primary">Landing page per vise</small> -->
+            
         </div>
     </div>
 </main>
