577fdd45d64293d45ba8a51a6e2a25de7615bb3e
Angelo Pavan <angelopavan.dev@gmail.com>
2023-05-06 10:29:02 +0200

    Modifica landing page.

--- src/_landing.php
+++ src/_landing.php
@@ -8,23 +8,34 @@
 <main class="d-flex align-items-end mt-5">
     <div class="container shadow-lg rounded-3 py-5 bg-white" style="min-height: 800vh;">
         <div class="row g-2 mt-5 bg-whitesmoke">
-
             <div class="col-10 mx-auto">
                 <div class="row">
                     <div class="col-6 my-auto">
                         <h2 class="text-center mb-4">Con <strong>Vise</strong> inviare denaro è semplice!</h2>
-                        <p>Effettua pagamenti verso chi desideri, che sia un amico o un esercente, l'unico requisito è avere un conto <strong>Vise</strong>.</p>
+                        <p>Effettua pagamenti verso chi desideri, che sia un amico o un negoziante, l'unico requisito è avere un conto <strong>Vise</strong>.</p>
                     </div>
                     
-                    <div class="col-6">
+                    <div class="col-6 text-center">
                         <img src="img/landing-image-phone.png" alt="Immagine Landing Page">
                     </div>
                 </div>
             </div>
-
-            <!-- <h1 class="display-3">Semplice... è Vise!</h1>
-            <small class="text-body-primary">Landing page per vise</small> -->
-            
         </div>
+
+        <div class="row g-2 mt-5">
+            <div class="col-10 mx-auto">
+                <div class="row">
+                    <div class="col-6 text-center">
+                        <img src="img/landing-image-security.png" alt="Immagine Landing Page" height="330px">
+                    </div>
+
+                    <div class="col-6 my-auto">
+                        <h2 class="text-center mb-4">Per <strong>Vise</strong> la sicurezza è importante.</h2>
+                        <p>Non rischiare la tua sicurezza finanziaria con aziende che non ti garantiscono la massima protezione dei tuoi dati bancari. Scegli <strong>Vise</strong>, il partner affidabile che mette al primo posto la tua sicurezza e ti permette di gestire i tuoi fondi con la massima tranquillità.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </main>
