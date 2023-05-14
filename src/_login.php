37ce00181327660cf9a5f219c3c9ac02cbdf3fef
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-05-14 15:37:15 +0200

    i'm going mental pls help

--- src/_login.php
+++ src/_login.php
@@ -1,4 +1,46 @@
 <?php
+if(isset($_SESSION['id'])){
+    header("Location: ?page=dashboard");
+}
+
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    echo "No token";
+} else {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $curl = curl_init();
+
+    curl_setopt_array(
+        $curl,
+        array(
+            CURLOPT_URL => 'localhost/Progetto_GPOI_VISE/src/API/API/login.php',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+                    "username_email":"' . $username . '",
+                    "password":"' . $password . '"
+                }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: text/plain'
+            ),
+        )
+    );
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    $json = json_decode($response);
+    if(isset($json->id)){
+        $_SESSION['id'] = $json->id;
+    }
+}
 
 ?>
 <title>Paga Vise | Checkout</title>
@@ -44,26 +86,7 @@
                 </form>
             </div>
         </div>
-        <?php
-        require 'vendor/autoload.php';
 
-        if (!isset($_POST['username']) || !isset($_POST['password'])) {
-            echo "No token";
-            return;
-        }
-
-        $client = new Client();
-        $headers = [
-            'Content-Type' => 'text/plain'
-        ];
-        $body = '{
-            "username_email":"admin@gmail.com",
-            "password":"admin"
-        }';
-        $request = new Request('GET', 'localhost/Progetto_GPOI_VISE/src/API/API/login.php', $headers, $body);
-        $res = $client->sendAsync($request)->wait();
-        echo $res->getBody();
-        ?>
         <script>
             (() => {
                 'use strict'
